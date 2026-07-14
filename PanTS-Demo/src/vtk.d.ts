1ec7b6435e335c3e25d1446cc7d771932edde448
harleensachdev <blackberezz@gmail.com>
2026-07-14 17:00:56 +0800

    Add ambient type declarations for vtk.js modules

---
+++ PanTS-Demo/src/vtk.d.ts
@@ -0,0 +1,3 @@
+declare module "@kitware/vtk.js/Common/DataModel/ImageData";
+declare module "@kitware/vtk.js/Common/Core/DataArray";
+declare module "@kitware/vtk.js/Filters/General/ImageMarchingCubes";
